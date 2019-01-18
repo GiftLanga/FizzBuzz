746b917a3367eeddaa57537ecaa3b4e0c73886e8
GiftLanga <glanga@retrorabbit.co.za>
2019-01-18 14:59:37 +0200

    FizzBuzz all files

---
+++ FizzBuzz.Test/FizzBuzzTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using FizzBuzz;
+
+
+namespace FizzBuzz.Test
+{
+    class FizzBuzzTest
+    {
+        FizzBuzzClass FZ;
+
+        public FizzBuzzTest()
+        {
+            FZ = new FizzBuzzClass();
+        }
+
+        [TestCase(15, "FizzBuzz")]
+        [TestCase(30, "FizzBuzz")]
+        [TestCase(27, "Fizz")]
+        [TestCase(10, "Buzz")]
+        [TestCase(11, "Whizz")]
+        [TestCase(5, "Buzz")]
+        [TestCase(7, "Whizz")]
+        [TestCase(1, "1")]
+        [TestCase(4, "4")]
+        [TestCase(73, "Whizz")]
+        [TestCase(2, "Whizz")]
+
+        public void givenNumber_Outcome(int number, string Outcome)
+        {
+            var result = FZ.isDivisible(number);
+            Assert.AreEqual(Outcome, result);
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzClass
+    {
+        public enum Outcome
+        {
+            Fizz,
+            Buzz,
+            FizzBuzz
+        }
+        public FizzBuzzClass()
+        {
+
+        }
+
+        public string isDivisible(int number)
+        {
+            if(isDivisiblebyBoth3and5(number))
+            {
+                return "FizzBuzz";
+            }
+            else if(isDivisibleby3(number))
+            {
+                return "Fizz";
+            }
+            else if (isDivisibleby5(number))
+            {
+                return "Buzz";
+            } else if (isPrime(number))
+            {
+                return "Whizz";
+            }
+            
+            return number.ToString();
+        }
+
+        public bool isDivisiblebyBoth3and5(int number) {
+            return number % 3 == 0 && number % 5 == 0;
+        }
+
+        public bool isDivisibleby3(int number) {
+            return number % 3 == 0;
+        }
+
+        public bool isDivisibleby5(int number) {
+            return number % 5 == 0;
+        }
+
+        public bool isPrime(int number)
+        {
+            if (number < 2) return false;
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if(number%i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
